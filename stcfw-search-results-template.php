b2bc5bb2202c0e5c8ec4da4e3b8731f3cbde9557
magenta-cuda <magenta.cuda@yahoo.com>
2015-10-02 22:39:29 -1000

    add comments for template use

--- stcfw-search-results-template.php
+++ stcfw-search-results-template.php
@@ -1,4 +1,8 @@
-<!-- Templates for stcfw-search-results-backbone.js - they will be compiled with _.template() -->
+<!-- Templates for stcfw-search-results-backbone.js - they will be compiled with _.template().    -->
+
+<!-- The id for gallery mouseover popups is "stcfw-template-" . $post_type . "-post_hover_view".  -->
+<!-- The "generic" template is used if no post type specific template is found.                   -->
+<!-- The fields in the Backbone model are the same fields as those displayed in the table format. -->
 
 <script type="text/html" id="stcfw-template-generic-post_hover_view">
 <div>
