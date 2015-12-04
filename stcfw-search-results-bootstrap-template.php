6b2033231690573d60c5b0e87f2dcb74884857bd
magenta-cuda <magenta.cuda@yahoo.com>
2015-12-03 16:11:46 -1000

    fix css problems

--- stcfw-search-results-bootstrap-template.php
+++ stcfw-search-results-bootstrap-template.php
@@ -1,6 +1,6 @@
 <!-- Bootstrap Backbone Gallery Template -->
 <script type="text/html" id="st_iv-bs-template_gallery">
-<div class="container">
+<div class="container st_iv-gallery">
     <div class="row">
         {{{ data.items }}}
     </div>
@@ -9,7 +9,7 @@
 <!-- Bootstrap Backbone Gallery Item Template -->
 <script type="text/html" id="st_iv-bs-template_gallery_item">
         <div class="col-sm-6 col-md-4 col-lg-3">
-            <figure class="item ems_xii-item">
+            <figure class="st_iv-gallery_item">
                 <figcaption><# print(stcfw.extractHrefAndLabelFromLink(data.post_title).label); #></figcaption>
                 <a href="<# print(stcfw.extractHrefAndLabelFromLink(data.post_title).href); #>" target="blank">
                     <img src="<# print(stcfw.extractHrefAndLabelFromLink(data.thumbnail).href); #>">
