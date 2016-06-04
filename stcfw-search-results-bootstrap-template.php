4e19f258f4b65a3d7f515b9f31ba4fdc99855b74
magenta-cuda <magenta.cuda@yahoo.com>
2016-06-04 13:06:57 -1000

    css tweaks

--- stcfw-search-results-bootstrap-template.php
+++ stcfw-search-results-bootstrap-template.php
@@ -15,7 +15,9 @@ This is done by instantiating these templates in .../js/stcfw-search-results-bac
 <script type="text/html" id="st_iv-bs-template_gallery_item">
         <div class="col-sm-6 col-md-4 col-lg-3">
             <figure class="img-rounded st_iv-gallery_item">
-                <figcaption><# print(stcfw.extractHrefAndLabelFromLink(data.post_title).label); #></figcaption>
+                <a href="<# print(stcfw.extractHrefAndLabelFromLink(data.post_title).href); #>" target="_blank">
+                    <figcaption><# print(stcfw.extractHrefAndLabelFromLink(data.post_title).label); #></figcaption>
+                </a>
                 <a href="<# print(stcfw.extractHrefAndLabelFromLink(data.post_title).href); #>" target="_blank">
                     <img src="<# print(stcfw.extractHrefAndLabelFromLink(data.thumbnail).href); #>">
                 </a>
