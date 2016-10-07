d7f2093000d78391ad8e38d0e1dd0d65aff7565a
magenta-cuda <magenta.cuda@yahoo.com>
2016-10-06 19:47:36 -1000

    better sample user templates

--- user-templates-sample.php
+++ user-templates-sample.php
@@ -4,11 +4,13 @@
   <thead>
     <tr>
       <th>Lake</th>
+      <th>Country</th>
       <th>Type</th>
       <th>Trophic</th>
       <th>Zone</th>
+      <th>Area</th>
+      <th>Depth</th>
       <th>Fishes</th>
-      <th>Excerpt</th>
     </tr>
   </thead>
   <tbody>
@@ -21,11 +23,13 @@
 <script type="text/html" id="st_iv-bs-template_table_item-lake">
     <tr>
       <td><a href="<# print(stcfw.extractHrefAndLabelFromLink(data.post_title).href); #>" target="_blank"><# print(stcfw.extractHrefAndLabelFromLink(data.post_title).label); #></</a></td>
+      <td><a href="<# print(stcfw.extractHrefAndLabelFromLink(data.country_id_of).href); #>" target="_blank"><# print(stcfw.extractHrefAndLabelFromLink(data.country_id_of).label); #></</a></td>
       <td>{{{ data["lake-type"] }}}</td>
       <td>{{{ data.trophic }}}</td>
       <td>{{{ data.zone }}}</td>
+      <td class="st_iv-table-cell-right-align">{{{ data.area }}}</td>
+      <td class="st_iv-table-cell-right-align">{{{ data.depth }}}</td>
       <td>{{{ data["river-fish"] }}}</td>
-      <td>{{{ data.post_content }}}</td>
     </tr>
 </script>
 
@@ -49,7 +53,7 @@
     <tr>
       <td><a href="<# print(stcfw.extractHrefAndLabelFromLink(data.post_title).href); #>" target="_blank"><# print(stcfw.extractHrefAndLabelFromLink(data.post_title).label); #></</a></td>
       <td>
-      <!-- the expression below is equivalent to <td>{{{ data.mountain_id_for }}}</td> but is provided as an example of doing loops in Backbone.js templates -->
+      <!-- the expression below is equivalent to <td>{{{ data.mountain_id_for }}}</td> but is provided as an example of parsing fields with multiple links -->
       <# stcfw.extractHrefAndLabelFromLinks(data.mountain_id_for).forEach(function(link,i){
           print((i?", ":"")+'<a href="'+link.href+'" target="_blank">'+link.label+'</a>');
       }); #>
